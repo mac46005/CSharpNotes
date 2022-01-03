23dc6aa324247bf25dcac573df970ef1740e2700
Marco Preciado <mac46005@gmail.com>
2022-01-02 21:32:32 -0600

    Using a delegate to call a different method

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ConsoleUI
 {
@@ -7,7 +8,7 @@ namespace ConsoleUI
         delegate void LogDel(string text);
         static void Main(string[] args)
         {
-            LogDel logDel = new LogDel(LogTextToScreen);
+            LogDel logDel = new LogDel(LogTextToFile);
 
             Console.WriteLine("Please enter you name:");
             var name = Console.ReadLine();
@@ -19,6 +20,12 @@ namespace ConsoleUI
 
         static void LogTextToScreen(string text){
             Console.WriteLine($"{DateTime.Now}: {text}");
+        }
+
+        static void LogTextToFile(string text){
+            using(StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"Log.txt"),true)){
+                sw.WriteLine($"{DateTime.Now}: {text}");
+            }
         }
     }
 }
