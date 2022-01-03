5f77a46594f4947bfba7f5a13f896c1336301c26
Marco Preciado <mac46005@gmail.com>
2022-01-02 21:17:18 -0600

    Simple use of a delegate

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,9 +4,21 @@ namespace ConsoleUI
 {
     class Program
     {
+        delegate void LogDel(string text);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LogDel logDel = new LogDel(LogTextToScreen);
+
+            Console.WriteLine("Please enter you name:");
+            var name = Console.ReadLine();
+
+            logDel(name);
+
+            Console.ReadKey();
+        }
+
+        static void LogTextToScreen(string text){
+            Console.WriteLine($"{DateTime.Now}: {text}");
         }
     }
 }
