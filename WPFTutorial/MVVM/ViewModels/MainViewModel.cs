b4276672bd9a83f559d97259a74d31cda14c417b
Marco Preciado <mac46005@gmail.com>
2022-01-05 15:37:51 -0600

    Created chat app using conventional non caliburn micro way

--- WPFTutorial/MVVM/ViewModels/MainViewModel.cs
+++ WPFTutorial/MVVM/ViewModels/MainViewModel.cs
@@ -4,19 +4,61 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPFTutorial.Core;
 using WPFTutorial.MVVM.Models;
 
 namespace WPFTutorial.MVVM.ViewModels
 {
-    class MainViewModel
+    class MainViewModel : ObservableObject
     {
         public ObservableCollection<MessageModel> Messages { get; set; }
         public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        /* Commands */
+        public RelayCommand SendCommand { get; set; }
+
+
+        private ContactModel contactModel;
+
+        public ContactModel SelectedContact
+        {
+            get { return contactModel; }
+            set 
+            {
+                contactModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _message;
+            
+        public string Message
+        {
+            get { return _message; }
+            set 
+            { 
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
 
         public MainViewModel()
         {
             Messages = new ObservableCollection<MessageModel>();
             Contacts = new ObservableCollection<ContactModel>();
+
+
+            SendCommand = new RelayCommand(o => {
+                Messages.Add(new MessageModel
+                {
+                    Message = Message,
+                    FirstMessage = false
+                });
+            });
+
+
+
 
             Messages.Add(new MessageModel
             {
