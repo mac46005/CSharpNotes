f6f224b97a61bc2f7136a258c0d6833b2b5b19a0
Marco Preciado <mac46005@gmail.com>
2022-01-15 10:34:12 -0600

    Setting up MVVM and styling procedures

---
+++ ModernFlatUITutorial/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ModernFlatUITutorial.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernFlatUITutorial.MVVM.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
